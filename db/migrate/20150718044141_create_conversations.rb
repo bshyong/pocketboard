5ecfeafe1232b55c6f07192e89c970dac5312232
Benjamin Shyong <ben.shyong@gmail.com>
2015-07-17 21:47:56 -0700

    convos and conversations

---
+++ db/migrate/20150718044141_create_conversations.rb
@@ -0,0 +1,19 @@
+class CreateConversations < ActiveRecord::Migration
+  def change
+    create_table :conversations do |t|
+      t.references  :user
+      t.string :status
+
+      t.timestamps
+    end
+
+    create_table :messages do |t|
+      t.string   :msg_id
+      t.string   :media_url
+      t.text     :body
+      t.references :conversation
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141125055736) do
+ActiveRecord::Schema.define(version: 20150718044141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "snaps", force: true do |t|
+  create_table "conversations", force: true do |t|
+    t.integer  "user_id"
+    t.string   "status"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: true do |t|
     t.string   "msg_id"
     t.string   "media_url"
-    t.integer  "user_id"
     t.text     "body"
+    t.integer  "conversation_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
