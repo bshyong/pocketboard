480c92ab5d25013a5e45b0c482c00318750fd374
Benjamin Shyong <ben.shyong@gmail.com>
2015-07-17 22:01:43 -0700

    style, table fields

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,9 +1,7 @@
 class PageController < ApplicationController
 
   def index
-    @count = Snap.count
-    @snaps = Snap.all
-    @users = User.all
+    @conversations = Conversation.all
   end
 
 end
